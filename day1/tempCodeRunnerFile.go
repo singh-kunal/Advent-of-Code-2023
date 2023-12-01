af6a4f596db87ac7ba24f3cbb832069b28ccf96b
Kunal Singh <kunaal2002002@gmail.com>
2023-12-02 00:02:39 +0530

    Day 1

---
+++ day1/tempCodeRunnerFile.go
@@ -0,0 +1,16 @@
+		// if '0' <= line[i] && line[i] <= '9' {
+			// 	str += string(line[i])
+			// 	break
+			// }
+			// for j := i - 1; j >= 0; j-- {
+			// 	substr := line[j:i]
+			// 	if val, found := ref[substr]; found {
+			// 		str += val
+			// 		flag = true
+			// 		break
+			// 	}
+			// }
+			// if flag {
+			// 	flag = false
+			// 	break
+			// }
